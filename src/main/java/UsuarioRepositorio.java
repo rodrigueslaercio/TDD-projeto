2622cb5d89710b5abbf9cb2db4a563fbca633c73
Lucas <lucas.g.oliveira1998@outlook.com>
2023-12-27 14:13:59 -0300

    Adicionando metodos auxiliares no repositorio de Usuarios

--- src/main/java/UsuarioRepositorio.java
+++ src/main/java/UsuarioRepositorio.java
@@ -9,6 +9,27 @@ public class UsuarioRepositorio {
         usuarios.add(usuario);
     }
 
+    public int obterIndice(Usuario usuarioAlvo) {
+        
+        for (int counter = 0; counter < this.usuarios.size(); counter++) {
+            if (this.usuarios.get(counter).equals(usuarioAlvo)) {
+                return counter;
+            }
+        }
+
+        return -1;
+    }
+
+    public void atualizar(int indice, Usuario novoUsuario) {
+
+        this.usuarios.set(indice, novoUsuario);
+    }
+
+    public Usuario obterUsuario(int indice) {
+
+        return this.usuarios.get(indice);
+    }
+
     public boolean verificarCpf(String cpf) {
         for(Usuario usuario : this.usuarios) {
             if (usuario.getCpf().equals(cpf)) {
@@ -18,4 +39,4 @@ public class UsuarioRepositorio {
 
         return false;
     }
- }
+}
